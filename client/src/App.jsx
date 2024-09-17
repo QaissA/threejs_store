8dbbe1faeaf71c51979ce8acd3ecf71d7c69b928
QaissA <qaissabdelhamid@gmail.com>
2024-09-18 00:55:23 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./pages/home";
+import Canvas from "./canvas";
+
+function App() {
+  return (
+    <main className="app trnasition-all ease-in">
+      <Home />
+      <Canvas />
+      {/* <Customizer /> */}
+    </main>
+  );
+}
+
+export default App;
